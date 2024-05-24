2a414c6c55abf5bd539f7aa0d5b7584bd2b6d2fa
Jeff Steward <steward.jeff@gmail.com>
2024-05-24 12:08:13 -0400

    scrub apikeys from the output

--- routes/data.js
+++ routes/data.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let URL = require('url');
 let HAM = require('@harvardartmuseums/ham');
 
 let ham = new HAM(process.env.HAM_APIKEY);
@@ -17,8 +18,21 @@ router.get('/:endpoint', async function(req, res, next) {
             qs.parameters[param] = req.query[param];
         }
     }
-
     let results = await ham.search(req.params.endpoint, qs.parameters, qs.aggregations);
+
+    // Rewrite the next and prev URLs according to the data routes used in this application
+    // Strip the HAM API key from the query string so it is not exposed across the app
+    if (results.info.next) {
+        let urlNext = URL.parse(results.info.next);
+        qNext = urlNext.query.substring(44);
+        results.info.next = `${req.protocol}://${req.get('host')}${req.baseUrl}/${req.params.endpoint}?${qNext}`;
+    }
+    if (results.info.prev) {
+        let urlPrev = URL.parse(results.info.prev);
+        qPrev = urlPrev.query.substring(44);
+        results.info.prev = `${req.protocol}://${req.get('host')}${req.baseUrl}/${req.params.endpoint}?${qPrev}`;
+    }
+    
     res.json(results);
 });
 
