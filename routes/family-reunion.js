ef12f4852a43f28c16ee587519a3d1f520e9cff3
haironghe03 <hairong.he03@gmail.com>
2024-06-05 13:56:58 -0400

    test

--- routes/family-reunion.js
+++ routes/family-reunion.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let HAM = require('@harvardartmuseums/ham');
+let exhibition_size = require('../data/exhibition_size');
 
 let applicationInfo = {
   title: 'Family Reunion',
@@ -13,10 +14,6 @@ let ham = new HAM(process.env.HAM_APIKEY);
 router.get('/', async function(req, res, next) {
   res.render('family-reunion/index', { about: applicationInfo });
 });
-
-router.get('/floor-3', async function(req, res, next) {
-  res.render('family-reunion/floor-3', {about: applicationInfo});
-})
 
 router.get('/poster', async function(req, res, next) {
   let params =  {
@@ -31,6 +28,9 @@ router.get('/poster', async function(req, res, next) {
   // process date here
 
   for (let i = 0; i < exhibition.length; i++){
+    // merge fields from exhibtions data with exhibition
+    exhibition[i].objectcount = exhibition_size.find(ex => ex.exhibitionid === exhibition[i].exhibitionid).objectcount;
+
     let begin = exhibition[i].begindate.split("-");
     exhibition[i].beginYear = parseInt(begin[0]);
     exhibition[i].beginMonth = parseInt(begin[1]);
@@ -50,5 +50,77 @@ router.get('/poster', async function(req, res, next) {
 
   res.render('family-reunion/poster', {layout: 'layout-family-reunion.hbs', about: applicationInfo, data: exhibition});
 });
+
+router.get('/floor-3', async function(req, res, next) {
+  let params =  {
+    venue: 'HAM',
+    after: 'begindate:2014-11-15',
+    size: 100,
+    sort : 'chronological'
+  };
+  let exhibition = await ham.Exhibitions.search(params);
+  exhibition = exhibition.records;
+  function notOnFloorThree(exhibit) {
+    return exhibit.venues[0].galleries[0].floor === "3";
+  }
+
+  exhibition = exhibition.filter(notOnFloorThree);
+
+  for (let i = 0; i < exhibition.length; i++){
+    // merge fields from exhibtions data with exhibition
+    exhibition[i].objectcount = exhibition_size.find(ex => ex.exhibitionid === exhibition[i].exhibitionid).objectcount;
+  
+    let begin = exhibition[i].begindate.split("-");
+    exhibition[i].beginYear = parseInt(begin[0]);
+    exhibition[i].beginMonth = parseInt(begin[1]);
+    
+    let end = exhibition[i].enddate.split("-");
+    exhibition[i].endYear = parseInt(end[0]);
+    exhibition[i].endMonth = parseInt(end[1]);
+  }
+
+  res.render('family-reunion/floor-3', {about: applicationInfo, data: exhibition});
+})
+
+router.get('/data', async function(req, res, next) {
+  let params =  {
+    venue: 'HAM',
+    after: 'begindate:2014-11-15',
+    size: 100,
+    sort : 'chronological'
+  };
+  let exhibition = await ham.Exhibitions.search(params);
+  exhibition = exhibition.records;
+  function notOnFloorThree(exhibit) {
+    return exhibit.venues[0].galleries[0].floor === "3";
+  }
+
+  exhibition = exhibition.filter(notOnFloorThree);
+  
+  for (let i = 0; i < exhibition.length; i++){
+    // merge fields from exhibtions data with exhibition
+    let e = exhibition_size.find(ex => ex.exhibitionid === exhibition[i].exhibitionid);
+    exhibition[i].objectcount = e.objectcount;
+  }
+
+  const structuredData = { name: "Galleries", children: [] };
+
+  exhibition.forEach(exhibit => {
+      exhibit.venues.forEach(venue => {
+        if (venue.galleries){
+          venue.galleries.forEach(gallery => {
+              let galleryId = gallery.galleryid;
+              let existingGallery = structuredData.children.find(g => g.name === galleryId);
+              if (!existingGallery) {
+                  existingGallery = { name: galleryId, children: [] };
+                  structuredData.children.push(existingGallery);
+              }
+              let exhibitionSize = exhibit.objectcount;
+              existingGallery.children.push({ name: exhibit.title, size: exhibitionSize });
+          });
+      }});
+  });
+  res.json(structuredData);
+})
 
 module.exports = router;
