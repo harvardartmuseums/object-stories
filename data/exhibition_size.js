2afc5c836aad63109700a1e52195b3cf79e828bb
haironghe03 <hairong.he03@gmail.com>
2024-06-08 17:25:08 -0400

    dynamic treemap version implemented, both treemaps with full annotations

---
+++ data/exhibition_size.js
@@ -0,0 +1,294 @@
+module.exports = [
+    {
+      "exhibitionid": 4867,
+      "objectcount": 7
+    },
+    {
+      "exhibitionid": 6322,
+      "objectcount": 7
+    },
+    {
+      "exhibitionid": 5201,
+      "objectcount": 9
+    },
+    {
+      "exhibitionid": 5022,
+      "objectcount": 15
+    },
+    {
+      "exhibitionid": 5065,
+      "objectcount": 15
+    },
+    {
+      "exhibitionid": 5610,
+      "objectcount": 16
+    },
+    {
+      "exhibitionid": 5064,
+      "objectcount": 18
+    },
+    {
+      "exhibitionid": 5755,
+      "objectcount": 18
+    },
+    {
+      "exhibitionid": 4972,
+      "objectcount": 19
+    },
+    {
+      "exhibitionid": 5045,
+      "objectcount": 19
+    },
+    {
+      "exhibitionid": 5653,
+      "objectcount": 21
+    },
+    {
+      "exhibitionid": 5231,
+      "objectcount": 23
+    },
+    {
+      "exhibitionid": 5245,
+      "objectcount": 25
+    },
+    {
+      "exhibitionid": 5058,
+      "objectcount": 30
+    },
+    {
+      "exhibitionid": 5756,
+      "objectcount": 30
+    },
+    {
+      "exhibitionid": 4834,
+      "objectcount": 31
+    },
+    {
+      "exhibitionid": 5238,
+      "objectcount": 31
+    },
+    {
+      "exhibitionid": 6395,
+      "objectcount": 34
+    },
+    {
+      "exhibitionid": 5486,
+      "objectcount": 35
+    },
+    {
+      "exhibitionid": 6321,
+      "objectcount": 35
+    },
+    {
+      "exhibitionid": 5830,
+      "objectcount": 36
+    },
+    {
+      "exhibitionid": 5164,
+      "objectcount": 37
+    },
+    {
+      "exhibitionid": 6243,
+      "objectcount": 37
+    },
+    {
+      "exhibitionid": 5836,
+      "objectcount": 38
+    },
+    {
+      "exhibitionid": 4768,
+      "objectcount": 39
+    },
+    {
+      "exhibitionid": 5668,
+      "objectcount": 40
+    },
+    {
+      "exhibitionid": 5700,
+      "objectcount": 40
+    },
+    {
+      "exhibitionid": 5385,
+      "objectcount": 41
+    },
+    {
+      "exhibitionid": 5834,
+      "objectcount": 41
+    },
+    {
+      "exhibitionid": 5081,
+      "objectcount": 42
+    },
+    {
+      "exhibitionid": 5151,
+      "objectcount": 42
+    },
+    {
+      "exhibitionid": 5924,
+      "objectcount": 42
+    },
+    {
+      "exhibitionid": 5450,
+      "objectcount": 43
+    },
+    {
+      "exhibitionid": 6194,
+      "objectcount": 44
+    },
+    {
+      "exhibitionid": 6323,
+      "objectcount": 45
+    },
+    {
+      "exhibitionid": 5244,
+      "objectcount": 46
+    },
+    {
+      "exhibitionid": 5573,
+      "objectcount": 47
+    },
+    {
+      "exhibitionid": 6219,
+      "objectcount": 49
+    },
+    {
+      "exhibitionid": 6284,
+      "objectcount": 50
+    },
+    {
+      "exhibitionid": 6158,
+      "objectcount": 52
+    },
+    {
+      "exhibitionid": 5908,
+      "objectcount": 53
+    },
+    {
+      "exhibitionid": 5925,
+      "objectcount": 55
+    },
+    {
+      "exhibitionid": 5803,
+      "objectcount": 59
+    },
+    {
+      "exhibitionid": 6209,
+      "objectcount": 61
+    },
+    {
+      "exhibitionid": 5114,
+      "objectcount": 62
+    },
+    {
+      "exhibitionid": 5034,
+      "objectcount": 65
+    },
+    {
+      "exhibitionid": 6390,
+      "objectcount": 65
+    },
+    {
+      "exhibitionid": 5660,
+      "objectcount": 67
+    },
+    {
+      "exhibitionid": 5699,
+      "objectcount": 70
+    },
+    {
+      "exhibitionid": 4983,
+      "objectcount": 73
+    },
+    {
+      "exhibitionid": 5295,
+      "objectcount": 75
+    },
+    {
+      "exhibitionid": 5117,
+      "objectcount": 77
+    },
+    {
+      "exhibitionid": 4991,
+      "objectcount": 79
+    },
+    {
+      "exhibitionid": 5909,
+      "objectcount": 83
+    },
+    {
+      "exhibitionid": 5116,
+      "objectcount": 89
+    },
+    {
+      "exhibitionid": 5630,
+      "objectcount": 91
+    },
+    {
+      "exhibitionid": 5902,
+      "objectcount": 92
+    },
+    {
+      "exhibitionid": 6286,
+      "objectcount": 92
+    },
+    {
+      "exhibitionid": 5635,
+      "objectcount": 93
+    },
+    {
+      "exhibitionid": 6242,
+      "objectcount": 94
+    },
+    {
+      "exhibitionid": 6294,
+      "objectcount": 95
+    },
+    {
+      "exhibitionid": 6120,
+      "objectcount": 105
+    },
+    {
+      "exhibitionid": 6233,
+      "objectcount": 108
+    },
+    {
+      "exhibitionid": 6324,
+      "objectcount": 120
+    },
+    {
+      "exhibitionid": 6265,
+      "objectcount": 131
+    },
+    {
+      "exhibitionid": 4916,
+      "objectcount": 133
+    },
+    {
+      "exhibitionid": 6261,
+      "objectcount": 138
+    },
+    {
+      "exhibitionid": 6182,
+      "objectcount": 151
+    },
+    {
+      "exhibitionid": 5995,
+      "objectcount": 153
+    },
+    {
+      "exhibitionid": 4830,
+      "objectcount": 165
+    },
+    {
+      "exhibitionid": 5388,
+      "objectcount": 165
+    },
+    {
+      "exhibitionid": 5877,
+      "objectcount": 177
+    },
+    {
+      "exhibitionid": 5615,
+      "objectcount": 185
+    }
+  ]
